2683df80e36892e67b54dae588a9b10c7c7afb84
kokasmark <mark.kokas04@gmail.com>
2024-11-04 18:04:57 +0100

    Alerts, new Build

---
+++ src/Alert.jsx
@@ -0,0 +1,55 @@
+import { Component } from "react";
+
+import { PiPlugsConnectedDuotone } from "react-icons/pi";
+import { PiPlugsDuotone } from "react-icons/pi";
+import { MdInfoOutline } from "react-icons/md";
+
+class Alert extends Component{
+    state={
+        alerts: []
+    }
+    fire(message, role, time) {
+        if (message.length === 0) return;
+    
+        let id = Math.random();
+    
+        // Use functional setState to ensure you're using the latest state
+        
+
+        setTimeout(() => {
+            this.setState(prevState => {
+                const offset = prevState.alerts.length*time;
+                const newAlerts = [...prevState.alerts, { message, role, time: time, id }];
+                return { alerts: newAlerts };
+            });
+            setTimeout(() => {
+                this.setState(prevState => {
+                    // Filter out the alert using the latest state
+                    const filteredAlerts = prevState.alerts.filter(a => a.id !== id);
+                    return { alerts: filteredAlerts };
+                });
+            }, time);
+        }, this.state.alerts.length*100);
+    
+        
+    }
+    
+    render(){
+        const {alerts} = this.state;
+       return(
+        <div className="alert-container">
+            {
+             alerts.map((alert,index) => (
+                <div className={`Alert ${alert.role}`} key={'alert-'+index} style={{animation: `alert-fade-out ${alert.time+10}ms linear`}}>
+                    {alert.role === "error" && <PiPlugsConnectedDuotone className="icon"/>}
+                    {alert.role === "success" && <PiPlugsDuotone className="icon"/>}
+                    {alert.role === "info" && <MdInfoOutline className="icon"/>}
+                    <p>{alert.message}</p>
+                </div>
+             ))   
+            }
+       </div>); 
+    }
+}
+
+export default Alert;
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { FaSatelliteDish } from 'react-icons/fa';
 import './App.css';
-import { Component, useEffect } from 'react'
+import { Component, createRef } from 'react'
 import { MdSatelliteAlt } from "react-icons/md";
 import Xarrow, { useXarrow } from 'react-xarrows';
 import { xml2js } from 'xml-js';
@@ -10,13 +10,14 @@ import { GiElectric } from 'react-icons/gi';
 import { GrSpectrum } from 'react-icons/gr';
 import { BsSpeedometer2 } from 'react-icons/bs';
 import { BiInfoCircle } from 'react-icons/bi';
+import Alert from './Alert';
 
 
 const Satellite = ({ satellite, index, parent }) => {
   return (
     <div className='satellite space-body' id={satellite.name}
       style={{
-        left: `${(1.0 + satellite.distance) * 100}vw`, zIndex: 10,
+        left: `${(1.0 + satellite.distance) * 100}%`, zIndex: 10,
         transform: `translateY(${satellite.offsetY}px) translateX(-10px)`,
         background: "black"
       }} onClick={() => parent.setState({ selected: satellite })}>
@@ -44,9 +45,10 @@ class App extends Component {
     signals: [],
     markers: [],
     selected: null,
-    isCtrlPressed:false,
+    isCtrlPressed: false,
     scrollDelta: 0
   }
+  alerts = createRef();
   loadTimeout = null;
   async dsnData() {
     const requestOptions = {
@@ -72,8 +74,6 @@ class App extends Component {
     let satellites = [];
     let signals = [];
 
-    console.log(dishes)
-
     dishes.forEach(dish => {
       let { target, _attributes } = dish;
       let targets = Array.isArray(target) ? target : [target];
@@ -84,7 +84,7 @@ class App extends Component {
           satellites.push({
             type: "satellite",
             name: attributes.name,
-            distance: parseFloat(attributes.rtlt) > -1.0 ? parseFloat(attributes.rtlt)/2 * 300000 / 149597870.7 : 0,
+            distance: parseFloat(attributes.rtlt) > -1.0 ? ((parseFloat(attributes.rtlt) * 300000) / 2) / 149597870.7 : 0,
             scale: 1,
             color: 'white',
             friendlyName: s.spacecraft.filter((s) => s._name === attributes.name.toLowerCase())[0]?._friendlyName,
@@ -94,7 +94,6 @@ class App extends Component {
         }
       });
       satellites = satellites.sort((a, b) => a.distance - b.distance);
-
       const threshold = 0.5; // Define the distance threshold for overlap
       const yOffsetIncrement = 100; // Define the offset increment for each satellite
 
@@ -118,20 +117,20 @@ class App extends Component {
         let downSignals = Array.isArray(downSignal) ? downSignal : [downSignal];
         downSignals.forEach(signal => {
           const signalAttributes = signal._attributes;
-          if(signals.filter((a) => a.start.includes(signalAttributes.spacecraft)).length == 0){
-          signals.push({
-            start: "Earth",
-            end: `${signalAttributes.spacecraft}`,
-            dataRate: signalAttributes.dataRate,
-            frequency: signalAttributes.frequency,
-            power: signalAttributes.power,
-            timeToReach: parseFloat(satellites.filter((a) => a.name == signalAttributes.spacecraft)[0].rtlt)/20
-          });
-        }
-          try{
+          if (signals.filter((a) => a.start.includes(signalAttributes.spacecraft)).length == 0) {
+            signals.push({
+              start: "Earth",
+              end: `${signalAttributes.spacecraft}`,
+              dataRate: signalAttributes.dataRate,
+              frequency: signalAttributes.frequency,
+              power: signalAttributes.power,
+              timeToReach: parseFloat(satellites.filter((a) => a.name == signalAttributes.spacecraft)[0].rtlt) / 20
+            });
+          }
+          try {
             let satellite = satellites.filter((a) => a.name == signalAttributes.spacecraft)[0];
-            satellite.signals = [...satellite.signals,signalAttributes];
-          }catch{
+            satellite.signals = [...satellite.signals, signalAttributes];
+          } catch {
 
           }
         });
@@ -141,54 +140,51 @@ class App extends Component {
         let upSignals = Array.isArray(upSignal) ? upSignal : [upSignal];
         upSignals.forEach(signal => {
           const signalAttributes = signal._attributes;
-          console.log(signalAttributes.power)
-          if(signals.filter((a) => a.end.includes(signalAttributes.spacecraft)).length == 0){
+          if (signals.filter((a) => a.end.includes(signalAttributes.spacecraft)).length == 0) {
             signals.push({
               start: "Earth",
               end: `${signalAttributes.spacecraft}`,
               dataRate: signalAttributes.dataRate,
               frequency: signalAttributes.frequency,
               power: signalAttributes.power,
-              timeToReach: parseFloat(satellites.filter((a) => a.name == signalAttributes.spacecraft)[0].rtlt)/20
+              timeToReach: parseFloat(satellites.filter((a) => a.name == signalAttributes.spacecraft)[0].rtlt) / 20
             });
           }
-          try{
+          try {
             let satellite = satellites.filter((a) => a.name == signalAttributes.spacecraft)[0];
-            satellite.signals = [...satellite.signals,signalAttributes];
-          }catch{
-            
+            satellite.signals = [...satellite.signals, signalAttributes];
+          } catch {
+
           }
         });
       }
     });
 
-    const markers =  Array.from({ length: Math.ceil(Math.max(...satellites.map(s => s.distance))) }, (_, i) => (i + 1))
+    if (this.alerts.current) {
+      this.alerts.current.fire("Data updated!","info",1000)
+    }
+    const markers = Array.from({ length: Math.ceil(Math.max(...satellites.map(s => s.distance))) }, (_, i) => (i + 1))
     this.setState({
       satellites,
       signals,
       markers
     });
-    if (!this.loadTimeout) {
-      this.loadTimeout = setTimeout(() => {
-        this.loadSatellites();
-      }, 5000);
-    }
   }
   getSignalOpacity(power) {
     power = parseFloat(power)
-    if(power === 0.0){
+    if (power === 0.0) {
       return 0.1;
     }
-    else if(power <= 2.5){
+    else if (power <= 2.5) {
       return 0.25;
     }
-    else if(power <= 5.0){
+    else if (power <= 5.0) {
       return 0.5;
     }
-    else if(power <= 7.5){
+    else if (power <= 7.5) {
       return 0.75;
     }
-    else if(power >= 10){
+    else if (power >= 10) {
       return 1.0;
     }
   }
@@ -199,11 +195,13 @@ class App extends Component {
     scrollContainer.addEventListener('wheel', (event) => {
       event.preventDefault();
       scrollContainer.scrollLeft += event.deltaY; // Applies vertical scroll delta to horizontal scroll
-      this.setState({scrollDelta: scrollContainer.scrollLeft/scrollContainer.scrollWidth})
+      this.setState({ scrollDelta: scrollContainer.scrollLeft / scrollContainer.scrollWidth })
     });
 
     window.addEventListener('keydown', this.handleKeyDown);
     window.addEventListener('keyup', this.handleKeyUp);
+
+    this.fetchPlanetData()
 
   }
   componentWillUnmount() {
@@ -213,62 +211,117 @@ class App extends Component {
   handleKeyDown = (event) => {
     // Check if the CTRL key is pressed
     if (event.ctrlKey) {
-        this.setState({ isCtrlPressed: true });
+      this.setState({ isCtrlPressed: true });
     }
-};
+  };
 
-handleKeyUp = (event) => {
+  handleKeyUp = (event) => {
     // Check if the CTRL key is released
     if (!event.ctrlKey) {
-        this.setState({ isCtrlPressed: false });
+      this.setState({ isCtrlPressed: false });
     }
-};
+  };
   scrollToBody(signal) {
     const scrollContainer = document.querySelector('.space');
     const element = document.getElementById(this.state.isCtrlPressed ? signal.start : signal.end);
     if (element) {
-        const rect = element.getBoundingClientRect();
-        
-        // Use only the horizontal (x-axis) scroll
-        scrollContainer.scrollTo({
-            left: rect.x ,
-            behavior: 'smooth'
-        });
-    }
-    this.setState({scrollDelta: scrollContainer.scrollLeft/scrollContainer.scrollWidth})
-}
+      const rect = element.getBoundingClientRect();
 
+      // Use only the horizontal (x-axis) scroll
+      scrollContainer.scrollTo({
+        left: rect.x,
+        behavior: 'smooth'
+      });
+    }
+    this.setState({ scrollDelta: scrollContainer.scrollLeft / scrollContainer.scrollWidth })
+  }
+
+  async fetchPlanetData() {
+    const planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+    const distances = [];
+
+    for (const planet of planets) {
+      const response = await fetch(`https://api.le-systeme-solaire.net/rest/bodies/${planet.toLowerCase()}`);
+      const data = await response.json();
+      distances.push({ name: planet, distanceFromSun: data.semimajorAxis / 149597870.7 }); // Convert km to AU
+    }
+    let updatedPlanets = this.state.planets;
+    for (var i = 0; i < distances.length; i++) {
+      updatedPlanets[i + 1].distance = distances[i].distanceFromSun
+    }
+    this.setState({ planets: updatedPlanets })
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.satellites !== this.state.satellites) {
+      const newSatellites = this.state.satellites;
+      const prevSatellites = prevState.satellites;
+
+      const newSatelliteNames = new Set(newSatellites.map(satellite => satellite.name));
+      const prevSatelliteNames = new Set(prevSatellites.map(satellite => satellite.name));
+
+      let count = 0;
+      // Check for newly connected satellites
+      newSatellites.forEach(satellite => {
+        if (!prevSatelliteNames.has(satellite.name)) {
+          if (this.alerts.current) {
+            this.alerts.current.fire(`${satellite.name} connected!`, "success", 2000+count*100);
+          }
+          count++;
+        }
+      });
+      count = 0;
+      // Check for disconnected satellites
+      prevSatellites.forEach(satellite => {
+        if (!newSatelliteNames.has(satellite.name)) {
+          if (this.alerts.current) {
+            this.alerts.current.fire(`${satellite.name} disconnected!`, "error", 2000+count*100);
+          }
+          count++;
+        }
+      });
+
+      if (!this.loadTimeout) {
+        this.loadTimeout = setTimeout(() => {
+          this.loadSatellites();
+          this.loadTimeout = null;
+        }, 5000);
+      }
+    }
+  }
 
   render() {
     const { selected } = this.state;
     return (
       <div className="App">
-        <div className='background' style={{left: `${-this.state.scrollDelta*200}%`}}/>
+        <div className='background' style={{ left: `${-this.state.scrollDelta * 200}%` }} />
         <div className='space'>
-          {this.state.markers.map((marker,index) =>(
-            <div style={{position: 'absolute', top: 0, left: `${marker*100}%`, 
-            color: 'white', width: 200, display: 'flex', 
-            flexDirection: 'column', justifyContent: 'center', textAlign: 'center', alignItems: 'center',transform: 'translateX(-75px)'}}>
-              <div style={{backgroundColor: '#4ba3eb', borderRadius: '0px 0px 50% 50%', padding: 20}}>
-                <h1 style={{margin: 0}}>{marker} AU</h1>
+          {this.state.markers.map((marker, index) => (
+            <div style={{
+              position: 'absolute', top: 0, left: `${marker * 100}%`,
+              color: 'white', width: 200, display: 'flex',
+              flexDirection: 'column', justifyContent: 'center', textAlign: 'center', alignItems: 'center', transform: 'translateX(-75px)'
+            }}>
+              <div style={{ backgroundColor: '#4ba3eb', borderRadius: '0px 0px 50% 50%', padding: 20 }}>
+                <h1 style={{ margin: 0 }}>{marker} AU</h1>
                 <p>({Math.round(marker * 149597870.7 / 1000000)} million km)</p>
               </div>
-              <span style={{width: 2, height: '100vh', backgroundColor: '#4ba3eb'}}></span>
+              <span style={{ width: 2, height: '100vh', backgroundColor: '#4ba3eb' }}></span>
             </div>
           ))}
-        {this.state.signals.map((signal, index) => (
-          <div className={`arrow`} info={`${signal.timeToReach}`} 
-          style={{opacity: this.getSignalOpacity(signal.power), animationDuration: `${signal.timeToReach}s`}} 
-          onClick={()=>this.scrollToBody(signal)}>
-            <Xarrow start={signal.start} end={signal.end} key={`signal-${index}`}
-              color={"#4ba3eb"}
-              path='smooth'
-              showHead={false}
-              strokeWidth={2}
-              curveness={0.8}
-            />
-          </div>
-        ))}
+          {this.state.signals.map((signal, index) => (
+            <div className={`arrow`} info={`${signal.timeToReach}`}
+              style={{ opacity: this.getSignalOpacity(signal.power), animationDuration: `${signal.timeToReach}s` }}
+              onClick={() => this.scrollToBody(signal)}>
+              <Xarrow start={signal.start} end={signal.end} key={`signal-${index}`}
+                color={"#4ba3eb"}
+                path='smooth'
+                showHead={false}
+                strokeWidth={2}
+                curveness={0.8}
+              />
+            </div>
+          ))}
           {
             this.state.satellites.map((satellite, index) => (
               <Satellite satellite={satellite} key={`satellite-${index}`} index={index} parent={this} />
@@ -286,7 +339,7 @@ handleKeyUp = (event) => {
           <div className='about'>
             <h1>Deep Space Network</h1>
             <h3>Interactive Map</h3>
-            
+
             <p>Distances are in Astronomical Units and they are in scale.
               Click on a signal's line and it will scroll from one end to the other.
               Click on a satellite to view various data by hovering the handle at the bottom of the page.
@@ -300,28 +353,28 @@ handleKeyUp = (event) => {
             <div className='satellite-info'>
               <h3>{selected.friendlyName}</h3>
               <img width={'90%'} style={{ borderRadius: 10 }} src={`https://eyes.nasa.gov/apps/dsn-now/images/spacecraft/${selected.friendlyName.toLowerCase().replace(" ", "%20")}.jpg`} />
-    
+
               <h3>Information</h3>
               <p><b>{selected.distance}</b> AU from Earth.</p>
               <p><b>{selected.rtlt}</b>s round trip of light.</p>
               <h3>Signal Information</h3>
               <div className='signals'>
                 {
-                  selected.signals.map((signal,index) => (
-                    <div className='signal' style={{opacity: signal.active === "true" ? 1 : 0.3}}>
+                  selected.signals.map((signal, index) => (
+                    <div className='signal' style={{ opacity: signal.active === "true" ? 1 : 0.3 }}>
                       <div className='data'><TbWaveSine /> <p><b>{parseFloat(signal.frequency)}</b> hz</p></div>
                       <div className='data'><GiElectric /> <p><b>{parseFloat(signal.power)}</b> dbm</p></div>
                       <div className='data'><GrSpectrum /> <p><b>{signal.band}</b> band</p></div>
-                      <div className='data'><BsSpeedometer2 /> <p><b>{parseFloat(signal.dataRate) > 1024 ? parseFloat(signal.dataRate)/1024 : parseFloat(signal.dataRate)}</b> {parseFloat(signal.dataRate) > 1024 ? 'kb/s' : 'b/s'}</p></div>
+                      <div className='data'><BsSpeedometer2 /> <p><b>{parseFloat(signal.dataRate) > 1024 ? parseFloat(signal.dataRate) / 1024 : parseFloat(signal.dataRate)}</b> {parseFloat(signal.dataRate) > 1024 ? 'kb/s' : 'b/s'}</p></div>
                       <div className='data'><BiInfoCircle /> <p>Type <b>{signal.signalType}</b></p></div>
                     </div>
                   ))
                 }
               </div>
             </div>
-          :<div className='satellite-info'><h1>No satellite selected.</h1></div>}
+            : <div className='satellite-info'><h1>No satellite selected.</h1></div>}
         </div>
-        
+        <Alert ref={this.alerts}/>
       </div>
     );
   }
